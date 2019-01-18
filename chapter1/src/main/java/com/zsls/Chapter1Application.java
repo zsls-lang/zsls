2aa32ab291c07a32e3ec9501a164ff8202fbefcd
zk1023lang <zk1023@163.com>
2019-01-18 15:40:41 +0800

    springboot详细配置

---
+++ chapter1/src/main/java/com/zsls/Chapter1Application.java
@@ -0,0 +1,40 @@
+package com.zsls;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
+/**
+ *@RestController 等同于 （@Controller 与 @ResponseBody）
+ */
+@RestController
+@SpringBootApplication
+public class Chapter1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter1Application.class, args);
+	}
+
+    @RequestMapping(value = "/demo1",method = RequestMethod.GET)
+	public String demo1(){
+		return "hello springboot";
+	}
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+		return args -> {
+			System.out.println("SpringBoot 默认提供的 Bean");
+			String[] beanDefinitionNames = ctx.getBeanDefinitionNames();
+			Arrays.stream(beanDefinitionNames).forEach(item-> System.out.println(item));
+		};
+	}
+
+}
+
--- chapter1/src/test/java/com/zsls/chapter1/Chapter1ApplicationTests.java
+++ chapter1/src/test/java/com/zsls/chapter1/Chapter1ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.zsls.zk.elasticserach;
+package com.zsls.chapter1;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TestelasticserachApplicationTests {
+public class Chapter1ApplicationTests {
 
 	@Test
 	public void contextLoads() {

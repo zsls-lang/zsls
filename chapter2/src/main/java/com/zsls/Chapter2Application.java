3afb5dfa4fd1a323c85faba53a07ea4b35983083
zk1023lang <zk1023@163.com>
2019-01-21 16:36:21 +0800

    springboot整合thymeleaf

---
+++ chapter2/src/main/java/com/zsls/Chapter2Application.java
@@ -0,0 +1,14 @@
+package com.zsls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter2Application.class, args);
+	}
+
+}
+

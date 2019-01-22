40910471c5dd41f0b01ef3df9d6a512952df4918
zk1023lang <zk1023@163.com>
2019-01-22 17:44:28 +0800

    springboot整合thymeleaf

--- chapter2/src/main/java/com/zsls/controller/ThymeleafController.java
+++ chapter2/src/main/java/com/zsls/controller/ThymeleafController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+
 
 @Controller
 @RequestMapping
@@ -26,6 +28,23 @@ public class ThymeleafController {
 		view.addObject(author);
 
 		return view;
+	}
+
+	@RequestMapping(value = "/index1",method = RequestMethod.GET)
+	public String index1(HttpServletRequest request){
+
+		// 设置属相
+		request.setAttribute("title","thymeleaf页面");
+		request.setAttribute("desc","welcome to thymeleaf");
+		Author author = new Author();
+		author.setAge(22);
+		author.setEmail("guanren@qq.com");
+		author.setName("柴进大官人");
+
+		// 设置跳转的视图 默认映射到 src\main\resources\templates\{viewName}.html
+		request.setAttribute("author",author);
+
+		return "index";
 	}
 
 

83d4aa1787714337f89ff9660170154627a2c4ae
zk1023lang <zk1023@163.com>
2019-03-18 21:57:14 +0800

    springboot使用websocket

---
+++ chapter17/src/main/java/com/zsls/Chapter17Application.java
@@ -0,0 +1,28 @@
+package com.zsls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+/**
+ * @Description //TODO
+ * 可以参考tomcat 里面的 apache-tomcat-8.5.33\webapps\examples\websocket 的html
+ * apache-tomcat-8.5.33\webapps\examples\WEB-INF\classes\websocket 的java
+ */
+@SpringBootApplication
+public class Chapter17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter17Application.class, args);
+	}
+
+	/**
+	 * @Description //
+	 * 首先要注入ServerEndpointExporter，这个bean会自动注册使用了@ServerEndpoint注解声明的Websocket endpoint
+	 */
+	@Bean
+	public ServerEndpointExporter serverEndpointExporter() {
+		return new ServerEndpointExporter();
+	}
+
+}

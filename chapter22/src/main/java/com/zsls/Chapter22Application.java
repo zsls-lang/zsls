f8968681dfe1691c0ae70214f2affe382ed1eb50
zk1023lang <zk1023@163.com>
2019-06-24 21:39:18 +0800

    elasticsearch基础学习

---
+++ chapter22/src/main/java/com/zsls/Chapter22Application.java
@@ -0,0 +1,13 @@
+package com.zsls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter22Application.class, args);
+	}
+
+}

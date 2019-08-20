1fedc9c1eca6e86a63560d16bdf219354e4d5f75
zk1023lang <zk1023@163.com>
2019-08-20 11:31:24 +0800

    spring事件模拟注册异步发送邮箱短信

--- chapter26/src/main/java/com/zsls/config/TaskConfig.java
+++ chapter26/src/main/java/com/zsls/config/TaskConfig.java
@@ -3,12 +3,14 @@ package com.zsls.config;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
 import java.util.concurrent.ThreadPoolExecutor;
 //https://www.cnblogs.com/jebysun/p/9675345.html
 @Configuration
-public class TaskConfig {
+public class TaskConfig implements SchedulingConfigurer {
     public TaskConfig() {
     }
 
@@ -36,4 +38,8 @@ public class TaskConfig {
         return taskExecutor;
     }
 
+	@Override
+	public void configureTasks(ScheduledTaskRegistrar scheduledTaskRegistrar) {
+		scheduledTaskRegistrar.setScheduler(taskExecutor());
+	}
 }

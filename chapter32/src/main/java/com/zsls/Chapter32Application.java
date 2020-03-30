3c421ab6f61f411c847d6429a58db103fe2d9873
zk1023lang <zk1023@163.com>
2020-03-30 17:58:14 +0800

    springboot整合kafka示例

---
+++ chapter32/src/main/java/com/zsls/Chapter32Application.java
@@ -0,0 +1,13 @@
+package com.zsls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter32Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter32Application.class, args);
+    }
+
+}
---
+++ chapter32/src/test/java/com/zsls/Chapter32ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.zsls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter32ApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}

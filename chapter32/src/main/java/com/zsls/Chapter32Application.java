657bdc81b902866662326258d2d467cf29ad8061
zk1023lang <zk1023@163.com>
2020-03-31 11:43:40 +0800

    springboot整合kafka测试

--- chapter32/src/main/java/com/zsls/Chapter32Application.java
+++ chapter32/src/main/java/com/zsls/Chapter32Application.java
@@ -1,13 +1,40 @@
 package com.zsls;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * http://www.kailing.pub/article/index/arcid/243.html
+ */
+@Slf4j
 @SpringBootApplication
+@RestController
 public class Chapter32Application {
+
 
     public static void main(String[] args) {
         SpringApplication.run(Chapter32Application.class, args);
+    }
+
+
+
+    @Autowired
+    private KafkaTemplate<Object, Object> template;
+
+    @GetMapping("/send/{input}")
+    public void sendFoo(@PathVariable String input) {
+        this.template.send("topic_input", input);
+    }
+    @KafkaListener(id = "webGroup", topics = "topic_input")
+    public void listen(String input) {
+        log.info("input value: {}" , input);
     }
 
 }

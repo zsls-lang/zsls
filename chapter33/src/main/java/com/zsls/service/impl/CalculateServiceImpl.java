98a8ecc506dce240fc34dc61d576367be83028bb
zk1023lang <zk1023@163.com>
2020-06-12 10:50:23 +0800

    ifelse之策略 Spring Boot

--- chapter33/src/main/java/com/zsls/service/impl/CalculateServiceImpl.java
+++ chapter33/src/main/java/com/zsls/service/impl/CalculateServiceImpl.java
@@ -1,26 +1,26 @@
 /**
- * Company
- * Copyright (C) 2004-2020 All Rights Reserved.
+ * Company Copyright (C) 2004-2020 All Rights Reserved.
  */
 package com.zsls.service.impl;
 
-import com.zsls.model.InputModel;
-import com.zsls.service.ICalculateService;
-import com.zsls.strategy.CalculateStrategy;
-import com.zsls.strategy.CalculatelHandlerOperationContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.zsls.model.InputModel;
+import com.zsls.service.ICalculateService;
+import com.zsls.strategy.CalculatelHandlerOperationContext;
+
 /**
  * @author zsls
- * @version $Id CalculateServiceImpl.java, v 0.1 2020-06-10 18:10  Exp $$
+ * @version $Id CalculateServiceImpl.java, v 0.1 2020-06-10 18:10 Exp $$
  */
 @Service
 public class CalculateServiceImpl implements ICalculateService {
-    @Autowired CalculatelHandlerOperationContext handlerOperationContext;
+    @Autowired
+    CalculatelHandlerOperationContext handlerOperationContext;
 
-    @Override public Double handleOrder(InputModel inputModel) {
-        CalculateStrategy calculateStrategy = handlerOperationContext.strategySelect(inputModel.getType());
-        return calculateStrategy.doOperation(inputModel);
+    @Override
+    public Double handleOrder(InputModel inputModel) {
+        return handlerOperationContext.strategySelect(inputModel.getType()).doOperation(inputModel);
     }
 }

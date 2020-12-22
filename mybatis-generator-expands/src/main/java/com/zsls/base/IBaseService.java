0cb26a697ee55364538bc40de1005f72e42e5bbe
zk1023lang <zk1023@163.com>
2020-12-22 18:04:47 +0800

    mybatis-generator 代码生成

--- mybatis-generator-expands/src/main/java/com/zsls/base/IBaseService.java
+++ mybatis-generator-expands/src/main/java/com/zsls/base/IBaseService.java
@@ -2,125 +2,149 @@ package com.zsls.base;
 
 import java.util.List;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
 /**
- *
  * @param <T> 实体类
  */
- public interface IBaseService<T> {
-	/**
-	 * 根据id查询数据
-	 *
-	 * @param id
-	 * @return
-	 */
-	 T queryById(Long id) ;
+public interface IBaseService<T> {
+    /**
+     * 根据id查询数据
+     *
+     * @param id
+     * @return
+     */
+    T queryById(Long id);
 
-	/**
-	 * 查询所有数据
-	 *
-	 * @return
-	 */
-	 List<T> queryList() ;
+    /**
+     * 查询所有数据
+     *
+     * @return
+     */
+    List<T> queryList();
 
-	/**
-	 * 根据条件查询一条数据，如果有多条数据会抛出异常
-	 *
-	 * @param record
-	 * @return
-	 */
-	 T queryUniqueResult(T record) ;
+    /**
+     * 根据条件查询一条数据，如果有多条数据会抛出异常
+     *
+     * @param record
+     * @return
+     */
+    T queryUniqueResult(T record);
 
-	/**
-	 * 根据条件查询数据列表
-	 *
-	 * @param record
-	 * @return
-	 */
-	 List<T> queryListByCondition(T record);
+    /**
+     * 根据条件查询数据列表
+     *
+     * @param record
+     * @return
+     */
+    List<T> queryListByCondition(T record);
 
-	/**
-	 * 分页查询
-	 *
-	 * @param page
-	 * @param rows
-	 * @param record
-	 * @return
-	 */
-	 PageResult<T> queryPageListByCondition(T record, Integer page, Integer rows) ;
-	/**
-	 * 分页查询
-	 *	排序
-	 * @param page
-	 * @param rows
-	 * @param record
-	 * @return
-	 */
-	PageResult<T> queryPageListOrderByCondition(T record, Integer page, Integer rows, String order) ;
+    /**
+     * 分页查询
+     *
+     * @param page
+     * @param rows
+     * @param record
+     * @return
+     */
+    PageResult<T> queryPageListByCondition(T record, Integer page, Integer rows);
 
-	/**
-	 * 新增数据，返回成功的条数
-	 *
-	 * @param record
-	 * @return
-	 */
-	 Integer save(T record);
+    /**
+     * 分页查询
+     * 排序
+     *
+     * @param page
+     * @param rows
+     * @param record
+     * @return
+     */
+    PageResult<T> queryPageListOrderByCondition(T record, Integer page, Integer rows, String order);
 
-	/**
-	 * 新增多条数据，返回成功的条数
-	 *
-	 * @param var1
-	 * @return
-	 */
-	 int insertList(List<T> var1);
+    /**
+     * 新增数据，返回成功的条数
+     *
+     * @param record
+     * @return
+     */
+    Integer save(T record);
 
-	/**
-	 * 新增数据，使用不为null的字段，返回成功的条数
-	 *
-	 * @param record
-	 * @return
-	 */
-	 Integer saveSelective(T record);
+    /**
+     * 新增多条数据，返回成功的条数
+     *
+     * @param var1
+     * @return
+     */
+    int insertList(List<T> var1);
 
-	/**
-	 * 修改数据，返回成功的条数
-	 *
-	 * @param record
-	 * @return
-	 */
-	 Integer update(T record);
+    /**
+     * 新增数据，使用不为null的字段，返回成功的条数
+     *
+     * @param record
+     * @return
+     */
+    Integer saveSelective(T record);
 
-	/**
-	 * 修改数据，使用不为null的字段，返回成功的条数
-	 *
-	 * @param record
-	 * @return
-	 */
-	 Integer updateSelective(T record);
+    /**
+     * 修改数据，返回成功的条数
+     *
+     * @param record
+     * @return
+     */
+    Integer update(T record);
 
-	/**
-	 * 根据id删除数据
-	 *
-	 * @param id
-	 * @return
-	 */
-	Integer deleteById(Long id);
+    /**
+     * 修改数据，使用不为null的字段，返回成功的条数
+     *
+     * @param record
+     * @return
+     */
+    Integer updateSelective(T record);
 
-	/**
-	 * 批量删除
-	 * @param clazz
-	 * @param property
-	 * @param values
-	 * @return
-	 */
-	Integer deleteByIds(Class<T> clazz, String property, List<Object> values) ;
+    /**
+     * 根据id删除数据
+     *
+     * @param id
+     * @return
+     */
+    Integer deleteById(Long id);
 
-	/**
-	 * 根据条件做删除
-	 *
-	 * @param record
-	 * @return
-	 */
-	Integer deleteByWhere(T record) ;
+    /**
+     * 批量删除
+     *
+     * @param clazz
+     * @param property
+     * @param values
+     * @return
+     */
+    Integer deleteByIds(Class<T> clazz, String property, List<Object> values);
 
+    /**
+     * 根据条件做删除
+     *
+     * @param record
+     * @return
+     */
+    Integer deleteByWhere(T record);
+
+    /**
+     * 集合查询
+     *
+     * @param record      查询参数
+     * @return 查询响应
+     */
+    List<T> list(T record);
+
+    /**
+     * 分页查询
+     *
+     * @param pageNum
+     * @param pageSize
+     * @param record      查询参数
+     * @return 分页集合
+     */
+    default PageInfo<T> page(T record,Integer pageNum, Integer pageSize) {
+        return PageHelper.startPage(pageNum,pageSize).doSelectPageInfo(() -> list(record));
+    }
 
 }

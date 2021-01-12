f660547d22c656cd997675e57dcf21637fff86c2
zk1023lang <zk1023@163.com>
2021-01-12 18:00:53 +0800

    spring boot quartz定时任务

---
+++ chapter34/src/main/java/com/zsls/Chapter34Application.java
@@ -0,0 +1,13 @@
+package com.zsls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter34Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter34Application.class, args);
+	}
+
+}
